150c35dbae40c1e4068e3d874064ae682ea600b3
sajicode <sajioloye@gmail.com>
2017-11-16 21:24:02 +0100

    Applying functions the right way

--- www/test.php
+++ www/test.php
@@ -1,42 +1,60 @@
 <?php
 
-    include("functions/function.php");
+	include('includes/functions.php');
 
-    define('MAX_FILE_SIZE', '2097152');
-    $ext = ['image/jpg', 'image/jpeg', 'image/png'];
+	define('MAX_FILE_SIZE', '2097152');
+	$ext = ['image/jpg', 'image/jpeg', 'image/png'];
 
-    if(array_key_exists('save', $_POST)) {
-        //print_r($_FILES);
+	if(array_key_exists('save', $_POST)) {
+		//print_r($_FILES);
 
-        $errors = array();      //initialize error array
+		$error = [];
 
-        $name = $_FILES['pics']['name'];
-        $size = $_FILES['pics']['size'];
+		if(empty($_FILES['pics']['name'])) {
+			$error[] = "Please select a file";
+		}
 
-        $tmp_name = $_FILES['pics']['tmp_name'];
+		if ($_FILES['pics']['size'] > MAX_FILE_SIZE) {
+			$error[] = "File too large. Maximum: ". MAX_FILE_SIZE;
+			$_FILES['pics']['tmp_name'] = null;
+		}
 
-        $file = picUpload($name, $size, $tmp_name);
+		if(!in_array($_FILES['pics']['type'], $ext)) {
+			$error[] = "File format not supported";
+		}
 
-        if(!in_array($_FILES['pics']['type'], $ext)) {   //validates for extension
-            $errors[] = "File format not supported";
-        }
+		/*$rnd = rand(0000000000, 9999999999);
+		$strip_name = str_replace(' ', '_', $_FILES['pics']['name']);
 
-        if(empty($errors)) {
-            echo "File upload successful";
-        } else {
-            foreach($errors as $err) {
-                echo $err.'<br/>';
+		$file_name = $rnd.$strip_name;
+		$destination = './uploads/'.$file_name; */
 
-            }
-        }
-    }
+		/* if(!move_uploaded_file($_FILES['pics']['tmp_name'], $destination)) {
+			$error[] = "File not uploaded";
+		} */
+
+		if(empty($error)) {
+			//move_uploaded_file($_FILES['pics']['tmp_name'], $destination);
+			$msg = uploadFile($_FILES, 'pics', 'uploads/');
+			//echo "File upload successful";
+			if($msg[0]) {
+				//echo $msg[0];
+				echo "File upload successful";
+			}
+		} else {
+			foreach ($error as $err) {
+				echo $err. '</br>';
+			}
+		}
+	}
+
 ?>
 
-<form id="register" method="POST", enctype="multipart/form-data">
+<form id="register" method="post" enctype="multipart/form-data">
+	<p>Please upload a picture</p>
+	<input type="file" name="pics">
 
-    <p>Please upload a picture</p>
-    <input type="file" name="pics"/>
-
-    <input type="submit" name="save"/>
+	<input type="submit" name="save">
+	
 
 </form>
