4594d9e37379ab146edabfa496fea7bdeeeba30d
sajicode <sajioloye@gmail.com>
2017-11-23 17:24:01 +0100

    modifying index.php

--- www/user/index.php
+++ www/user/index.php
@@ -1,6 +1,17 @@
 <?php
 
+    include("includes/db.php");
+
+    include("includes/functions.php");
+
     include("includes/header.php");
+
+?>
+
+
+
+
+<?php
 
     include("includes/footer.php");
 
